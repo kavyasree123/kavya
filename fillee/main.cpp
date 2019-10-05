4c57fc854675d79630f22b3224dcb3423acc760b
kavyasree123 <kavyasreemoni@gmail.com>
2019-10-05 12:35:05 +0530

    my life first commit

---
+++ fillee/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include"functions.h"
+using namespace std;
+int main()
+{
+story();
+cout<<endl;
+cout<<"factorial of num is"<<factorial;
+
+return 0;
+}
---
+++ fillee/story.cpp
@@ -0,0 +1,9 @@
+
+#include"functions.h"
+#include<iostream>
+using namespace std;
+void story()
+{
+cout<<"kavya sree";
+}
+
